c8503cb572f82cb8dfb5567c689751d120b123b8
razanelsayegh-sys <razanelsayegh@gmail.com>
2026-09-20 15:22:49 -0400

    Added the window+ functions for the Skyline+ functions for the lights of the skylines
    I did the buildings with their window lights.

--- Prototype 1 city lights/js/script.js
+++ Prototype 1 city lights/js/script.js
@@ -21,6 +21,7 @@ function setup() {
 function draw() {
     drawWindow();
     drawSkyline();
+    drawlights();
 
 }
 /**
@@ -41,4 +42,93 @@ function drawWindow(){
     fill('navy');
     rect(120,90,460,210);
  pop();
+}
+/**
+ * Draw the skyline of the city outside the window
+ */
+function drawSkyline() {
+    // Draw the city buildings(medium height)
+    push();
+    noStroke();
+    fill('gray');
+    rect(120, 180, 50, 120);
+    rect(220, 160, 60, 140);
+    rect(320, 180, 50, 120);
+    rect(420, 180, 50, 120);
+    rect(520, 180, 60, 120);
+    pop();
+
+    // Draw the city buildings(tall height)
+    push();
+    noStroke();
+    fill('dimgray');
+    rect(170, 110, 60, 190);
+    rect(270, 130, 50, 170);
+    rect(370, 120, 50, 180);
+    rect(470, 145, 55, 155);
+    pop();
+// Draw the city buildings(short height)
+    push();
+    noStroke();
+    fill('darkgray');
+    rect(150, 220, 50, 80);
+    rect(250, 220, 50, 80);
+    rect(350, 220, 50, 80);
+    rect(450, 220, 70, 80);
+    pop();
+}
+function drawlights(){
+    // Draw the lights in the buildings
+    push();
+    noStroke();
+    fill('yellow');
+    // lights in the medium height buildings
+    //lights medium building 1
+    rect(130, 190, 10, 10);
+    //lights medium building 2
+    rect(240, 170, 10, 10);
+    //lights medium building 3
+    rect(330, 190, 10, 10);
+    //lights medium building 4
+    rect(430, 190, 10, 10);
+    //lights medium building 5
+    rect(530, 190, 10, 10);
+    rect(560, 230, 10, 10);
+    pop();
+
+    // lights in the tall height buildings
+    push();
+    noStroke();
+    fill('yellow');
+    //lights tall building 1
+    rect(180, 120, 10, 10);
+    rect(180, 150, 10, 10);
+    rect(180, 180, 10, 10);
+    //lights tall building 2
+    rect(280, 140, 10, 10);
+    //lights tall buildings 3
+    rect(380, 130, 10, 10);
+    rect(380, 160, 10, 10);
+    rect(400, 180, 10, 10);
+    //lights tall buildings 4
+    rect(480, 155, 10, 10);
+    pop();
+
+    // lights in the short height buildings
+    push();
+    noStroke();
+    fill('yellow');
+    //lights short building 1
+    rect(160, 230, 10, 10);
+    rect(180, 250, 10, 10);
+    //lights short building 2 
+    rect(260, 230, 10, 10);
+    rect(260, 250, 10, 10);
+    //lights short building 3
+    rect(360, 230, 10, 10);
+    //lights short building 4
+    rect(460, 230, 10, 10);
+    rect(500, 230, 10, 10);
+    rect(500, 260, 10, 10);
+    pop();
 }
