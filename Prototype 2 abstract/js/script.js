359c89dc6d6ba959a4dfced34defe7398db6e2f0
razanelsayegh-sys <razanelsayegh@gmail.com>
2026-09-20 23:08:32 -0400

    Created the canvas and added all the functions needed to create an abstract drawing
    I started coding all the elements that are inside the canvas

--- Prototype 2 abstract/js/script.js
+++ Prototype 2 abstract/js/script.js
@@ -1,24 +1,63 @@
 /**
- * Title of Project
- * Author Name
+ * Abstract 
+ * Razan Elsaygh
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ *Creating a abstract art piece with diffrent geometrical formes and colors.
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Ceating the canvas 
 */
 function setup() {
-
+    createCanvas(400,400)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Drawing the abstract art piece
 */
 function draw() {
+    drawbackground();
+    drawTriangle();
+    drawCircle();
+}
 
+/**
+ * Drawing the background
+*/
+function drawbackground() {
+    push();
+    background(255, 105, 173);
+    pop();
+}
+/**
+ * Drawing the background triangle
+ */
+function drawTriangle(){
+// First triangle 
+    push();
+    fill(255, 200, 0);
+    noStroke();
+    triangle(10, 50, 400, 10, 150, 390);
+    pop();
+//Second triangle with transparency
+    push();
+    fill(255, 165, 0, 150);
+    noStroke();
+    triangle(20, 10, 50, 380, 400, 280);
+    pop();
+//Third triangle with transparency
+    push();
+    fill(255, 210, 0, 150);
+    noStroke();
+    triangle(320, 5, 10, 150, 350, 400);
+    pop();
+//Fourth triangle with transparency
+    push();
+    fill(255, 210, 0, 110);
+    noStroke();
+    triangle(410, 105, 0, 340, 250, 0);
+    pop();
 }
